a69d7769f5cd41f0c1365207a23740dd87830fc0
tipsumie <89018867+tipsumie@users.noreply.github.com>
2022-03-09 15:06:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
+import { Card, CardContent, FormControl, MenuItem, Select } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import /* It's a function that takes in a `province` and returns a `component` */
+Map from './Map';
+import InfoBox from './InfoBox';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [provinces, setProvinces] = useState([]);
+  const [province, setProvince] = useState("thailand");
+  const [provinceInfo, setProvinceInfo] = useState({});
+
+  useEffect(() => {
+    fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all")
+      .then((response) => response.json())
+      .then((data) => {
+        setProvinceInfo(data);
+
+      });
+  }, []);
+
+  console.log('data_All...', provinceInfo);
+
+  useEffect(() => {
+    //async -> send a request, wait for it, do something with 
+   const getProvincesData = async  () => {
+     fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces")
+      .then((response) => response.json())
+      .then((data) => {
+        const provinces = data.map((province) => ({
+          name: province.province
+        }));
+        setProvinces(provinces);
+
+      });
+   };
+  
+    getProvincesData();
+  }, []); 
+
+  const onProvinceChange = async (event) =>{
+    const provinceCode = event.target.value;
+
+    const url = 
+      provinceCode === "thailand"
+       ? "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+       : 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces' ;
+  
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //find the element with a given name in an array and return just one item (namely the first match).
+      const provinceInfo = data.find(item => item.province === provinceCode);
+
+        setProvince(provinceCode);
+      // All of the data from the province response
+        setProvinceInfo(provinceInfo);
+        });
+    
+    };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__left'>
+        <div className="app__header">
+          <h1>COVID-19 TRACKER in Thailand</h1>
+          <FormControl>
+            <Select variant="outlined" value={province} onChange={onProvinceChange}>
+              <MenuItem value="thailand">ประเทศไทย</MenuItem>
+              {provinces.map((province) => (
+                <MenuItem value={province.name}>{province.name}</MenuItem>     // Loop through all the province and show drop down list of the option 
+              ))}
+
+            </Select>
+          </FormControl>
+       </div>
+
+        <div className='app__stats'>
+          <InfoBox 
+          title='Coronavirus Cases' 
+          cases={provinceInfo.new_case} 
+          total={provinceInfo.total_case} 
+          />
+          <InfoBox 
+          title='Recovred' 
+          cases={provinceInfo.new_recovered} 
+          total={provinceInfo.total_recovered} 
+          />
+          <InfoBox 
+          title='Deaths' 
+          cases={provinceInfo.new_death} 
+          total={provinceInfo.total_death} 
+          />
+        </div>
+      
+        <Map/>
+      </div>
+      
+      <Card className='app_right'>
+        <CardContent>
+          <h3>Live New Cases by Province</h3>
+          {/*Table */}
+          <h3>Thailand New Cases</h3>
+          {/*Graph */}
+
+        </CardContent>
+      </Card>
+
     </div>
+  
+
+  
   );
 }
 
---
+++ src/InfoBox.js
@@ -0,0 +1,25 @@
+import { Card, CardContent, Typography } from '@mui/material'
+import React from 'react'
+
+function infoBox({title, cases, total}) {
+  return (
+    <Card className='infoBox'>
+        <CardContent>
+            {/*Title*/ }
+            <Typography className='infoBox__title' color="textSecondary">
+                {title}
+            </Typography>
+
+            {/*Numbber of cases*/ }
+            <h2 className='infoBox__cases'>{cases}</h2>
+
+            {/*Total of case*/ }
+            <Typography className='infoBox__total' color="textSecondary">
+                {total} total
+            </Typography>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default infoBox
